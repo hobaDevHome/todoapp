0be4c732d4b2716a373c7f10752c31048b8fd285
Heba <heba96@yahoo.com>
2024-07-03 14:21:42 +0300

    todo item styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,31 +1,36 @@
 import React from "react";
-import { FaTrash, FaCheck } from "react-icons/fa";
+
+import { FaTrash, FaCheck, FaListUl } from "react-icons/fa";
+import { CiViewList } from "react-icons/ci";
 import "./TodoItem.css";
 
 function TodoItem({ todo, onRemove, onToggle }) {
   console.log("inside todo item", todo.id);
   return (
-    <li className={`todo-item ${todo?.completed ? "completed" : ""}`}>
-      <div>
-        <span
-          style={{ textDecoration: todo?.completed ? "line-through" : "none" }}
-          onClick={() => onToggle(todo.id)}
-          className="todo-title"
-        >
-          {todo?.title}
-        </span>
-        <p className="todo-description">{todo?.description}</p>
+    <li
+      className={`todo-item ${todo?.completed ? "completed" : "incompleted"}`}
+      onClick={() => onToggle(todo.id)}
+    >
+      <div className="todo-data">
+        <CiViewList className="list-icon" />
+        <div className="todo-details">
+          <p className="todo-title">{todo?.title}</p>
+          <p className="todo-description">{todo?.description}</p>
+        </div>
       </div>
-      <FaCheck
-        className="check-icon"
-        onClick={() => onToggle(todo.id)}
-        style={{ color: todo?.completed ? "green" : "grey" }}
-      />
-      <FaTrash
-        className="trash-icon"
-        onClick={() => onRemove(todo.id)}
-        style={{ color: "red" }}
-      />
+      <div className="todo-item-icons">
+        <div
+          className="check-icon-bg"
+          style={{ backgroundColor: !todo?.completed ? "#fff" : "#25d37f" }}
+        >
+          <FaCheck
+            className="check-icon"
+            onClick={() => onToggle(todo.id)}
+            style={{ color: todo?.completed ? "green" : "grey" }}
+          />
+        </div>
+        <FaTrash className="trash-icon" onClick={() => onRemove(todo.id)} />
+      </div>
     </li>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,17 +2,34 @@ import React from "react";
 import TodoItem from "./TodoItem";
 
 function TodoList({ todos, onRemove, onToggle }) {
+  const incompleteTodos = todos.filter((todo) => !todo.completed);
+  const completeTodos = todos.filter((todo) => todo.completed);
+
   return (
-    <ul>
-      {todos?.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          onRemove={onRemove}
-          onToggle={onToggle}
-        />
-      ))}
-    </ul>
+    <div>
+      <h2>Todos</h2>
+      <ul className="todo-list incomplete">
+        {incompleteTodos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onRemove={onRemove}
+            onToggle={onToggle}
+          />
+        ))}
+      </ul>
+      <h2>Completed</h2>
+      <ul className="todo-list complete">
+        {completeTodos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onRemove={onRemove}
+            onToggle={onToggle}
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
 
